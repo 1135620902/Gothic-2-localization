163ae858ced4a34776f3b9964ef9daf1c8727c2b
1135620902 <143614951+1135620902@users.noreply.github.com>
2024-06-15 10:16:10 +0800

    Update BookstandAlchemy.d

--- Scripts/Content/Story/Dialog_Mobsis/BookstandAlchemy.d
+++ Scripts/Content/Story/Dialog_Mobsis/BookstandAlchemy.d
@@ -38,7 +38,7 @@ func void Use_BookstandALCHEMY1_S1() // Alchemiebücher
 		Doc_SetPage(nDocID, 1, "Book_Mage_R.tga", 0);
 
 		Doc_SetFont(nDocID, -1, FONT_Book);
-		Doc_SetMargins(nDocID, 0, 350, 20, 30, 20, 1);
+		Doc_SetMargins(nDocID, 0, 300, 20, 30, 20, 1);
 
 		Doc_PrintLine(nDocID, 0, "");
 		Doc_PrintLine(nDocID, 0, "");
@@ -56,7 +56,7 @@ func void Use_BookstandALCHEMY1_S1() // Alchemiebücher
 		Doc_PrintLine(nDocID, 0, BookstandALCHEMY1_S1_5);
 		Doc_PrintLine(nDocID, 0, "");
 
-		Doc_SetMargins(nDocID, -1, 30, 20, 350, 20, 1);
+		Doc_SetMargins(nDocID, -1, 30, 20, 300, 20, 1);
 		Doc_PrintLine(nDocID, 1, "");
 		Doc_PrintLine(nDocID, 1, "");
 		Doc_PrintLines(nDocID, 1, BookstandALCHEMY1_S1_6);
@@ -100,7 +100,7 @@ func void Use_BookstandALCHEMY2_S1() // Alchemiebücher
 		Doc_SetPage(nDocID, 1, "Book_Mage_R.tga", 0);
 
 		Doc_SetFont(nDocID, -1, FONT_Book);
-		Doc_SetMargins(nDocID, 0, 275, 20, 30, 20, 1);
+		Doc_SetMargins(nDocID, 0, 300, 20, 30, 20, 1);
 
 		Doc_PrintLine(nDocID, 0, "");
 		Doc_PrintLine(nDocID, 0, "");
@@ -118,7 +118,7 @@ func void Use_BookstandALCHEMY2_S1() // Alchemiebücher
 		Doc_PrintLine(nDocID, 0, BookstandALCHEMY2_S1_5);
 		Doc_PrintLine(nDocID, 0, "");
 
-		Doc_SetMargins(nDocID, -1, 30, 20, 275, 20, 1);
+		Doc_SetMargins(nDocID, -1, 30, 20, 300, 20, 1);
 		Doc_PrintLine(nDocID, 1, "");
 		Doc_PrintLine(nDocID, 1, "");
 		Doc_PrintLines(nDocID, 1, BookstandALCHEMY2_S1_6);
