a930f896db131c15a3097179890f1eeb2cb17b1b
1135620902 <143614951+1135620902@users.noreply.github.com>
2024-06-30 16:50:30 +0800

    Update IT_Addon_Written.d

--- Scripts/Content/Items/IT_Addon_Written.d
+++ Scripts/Content/Items/IT_Addon_Written.d
@@ -323,7 +323,7 @@ func void Use_MCELIXIER_01()
 	Doc_SetPages(nDocID, 1); // wieviel Pages
 	Doc_SetPage(nDocID, 0, "letters.TGA", 0);
 				Doc_SetFont(nDocID, 0, FONT_BookHeadline); // -1 -> all pages
-				Doc_SetMargins(nDocID, -1, 50, 50, 50, 50, 1); // 0 -> margins are in pixels
+				Doc_SetMargins(nDocID, -1, 70, 50, 50, 50, 1); // 0 -> margins are in pixels
 				Doc_PrintLine	(nDocID, 0, "");
 				Doc_SetFont(nDocID, 0, FONT_Book); // -1 -> all pages
 				Doc_PrintLine	(nDocID, 0, MCELIXIER_01_1);
