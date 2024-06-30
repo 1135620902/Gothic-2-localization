5bec24bbb386ae825d539738d1c390be6d6e34b7
1135620902 <143614951+1135620902@users.noreply.github.com>
2024-06-30 16:53:26 +0800

    Update IT_Addon_Written.d

--- Scripts/Content/Items/IT_Addon_Written.d
+++ Scripts/Content/Items/IT_Addon_Written.d
@@ -427,7 +427,7 @@ func void Use_Joint_Rezept_01()
 	Doc_SetPages(nDocID, 1); // wieviel Pages
 	Doc_SetPage(nDocID, 0, "letters.TGA", 0);
 				Doc_SetFont(nDocID, 0, FONT_BookHeadline); // -1 -> all pages
-				Doc_SetMargins(nDocID, -1, 50, 50, 50, 50, 1); // 0 -> margins are in pixels
+				Doc_SetMargins(nDocID, -1, 70, 50, 50, 50, 1); // 0 -> margins are in pixels
 				Doc_PrintLine	(nDocID, 0, Joint_Rezept_01_1);
 				Doc_SetFont(nDocID, 0, FONT_Book); // -1 -> all pages
 				Doc_PrintLine	(nDocID, 0, "");
